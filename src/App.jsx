23fe511f7d4884467119f4ea6787cf0b9e07d75f
Gabriel <grabiel.dias@arcanjoecompany.com>
2024-05-09 09:05:41 -0300

    subindo os codigos

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useRef, useState } from "react"
+import { Task } from "./components/Task"
+
+import styles from './App.module.css';
+
+
+export function App(){
+  const inputRef = useRef(null)
+  const [tasks, setTasks] = useState([])
+  const [inputClassName, setInputClassName] = useState(styles.input) 
+
+  
+
+  function handleAddTask(){
+
+    if(inputRef.current.value == ''){
+      setInputClassName(styles.inputError)
+    } 
+
+    else{
+      setInputClassName(styles.input)
+      const newTask = {
+        id: tasks.length + 1,
+        title: inputRef.current.value,
+        isCompleted:false
+      }
+      setTasks([...tasks, newTask])
+  
+      inputRef.current.value = ''
+
+      
+    }
+
+  }
+
+  function taskCompleted(id){
+    const taskIndex = tasks.findIndex(item => item.id == id)
+
+    if(taskIndex == -1){
+      return
+    }
+
+    const newTasks = [...tasks]
+    newTasks[taskIndex].isCompleted = true
+
+    setTasks(newTasks)
+
+  }
+
+  function removeTask(id){
+    const taskIndex = tasks.filter(item => item.id !== id)
+    setTasks(taskIndex)
+  }
+
+  function removeAll(){
+    setTasks([]);
+  }
+  
+  return(
+    <main className={styles.container}>
+      <h1 className={styles.title}>Todo App</h1>
+
+      <div className={styles.inputGroup}>
+        <input className={inputClassName} ref={inputRef} type="text" placeholder="Digite sua tarefa"/>
+        <button className={styles.button}   onClick={handleAddTask}>Add</button>
+        <button className={styles.buttonClean} onClick={removeAll}>Limpar</button>
+      </div>
+
+      <div className={styles.tasks}>
+
+        {tasks.map(item => (
+          <Task key ={item.id} task = {item} taskCompleted={taskCompleted} removeTask={removeTask}/>
+        ))}
+
+        {!tasks.length && <p>Nenhuma tareda ainda...</p>}
+
+      </div>
+
+    </main>
+  )
+}
---
+++ src/components/Task/index.jsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css'
+
+
+export function Task(props){
+
+    function viewDate(){
+        const date = new Date();
+        const formatDate= `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+        return formatDate
+    }
+
+    return(
+        <div className={styles.container}>
+            <p className={`${styles.title} ${props.task.isCompleted && styles.completed}`} >{props.task.id}. {props.task.title}</p> 
+            <p>{viewDate()}</p>
+            <div>
+                {!props.task.isCompleted && (
+                    <button onClick={() => props.taskCompleted(props.task.id)} className={styles.button}>Ok</button>
+                )}
+                {!props.task.isCompleted && (
+
+                    <button onClick={() => props.removeTask(props.task.id)} className={styles.removeButton}>Apagar</button>
+
+                )}
+            </div>
+        </div>
+    )
+
+}
